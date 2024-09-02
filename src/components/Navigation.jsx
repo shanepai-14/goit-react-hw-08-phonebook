8ed5f3f80215a8bd52de98a8eacdebef731adfaa
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-09-02 12:03:31 +0800

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,7 @@ import { AppBar, Toolbar, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const Navigation = () => (
-  <AppBar position="static">
+  <AppBar position="static" color="success">
     <Toolbar>
       <Box sx={{ flexGrow: 1 }}>
         <Button component={Link} to="/goit-react-hw-08-phonebook" color="inherit">Home</Button>
